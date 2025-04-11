b6bf0e4451cde94d0256d257c9a7d0c41cf42c16
chao <156628952+ShenCodr@users.noreply.github.com>
2025-04-11 12:13:37 +0800

    Update Union_find.cpp

--- Graph_theory/Union_find.cpp
+++ Graph_theory/Union_find.cpp
@@ -8,12 +8,10 @@ using namespace std;
 class Union_find {
 private:
 	vector<int> parent;//父节点
-	vector<int> ranksize;//树的高度
 
 public:
 	Union_find(int n) {//初始化
 		parent.resize(n);
-		ranksize.resize(n, 0);
 		for (int i = 0; i < n; i++) {
 			parent[i] = i;
 		}
@@ -26,20 +24,13 @@ public:
 			return parent[x];
 	}
 
-	void merge(int x, int y) {//合并算法
-		int rootx = find(x);//定义x的根节点
-		int rooty = find(y);//定义y的根节点
-		if (rootx == rooty) return;//如果x和y的根节点相同，直接返回
-		else if (ranksize[rootx] > ranksize[rooty]) {//谁大跟谁
-			parent[rooty] = rootx;
-		}
-		else if (ranksize[rootx] < ranksize[rooty]) {//谁大跟谁
-			parent[rootx] = rooty;
-		}
-		else {//一样高
-			parent[rooty] = rootx;
-			ranksize[rootx]++;
-		}
+	void merge(int x, int y) {
+             int rootx = find(x); // 找到 x 的根
+             int rooty = find(y); // 找到 y 的根
+           if (rootx == rooty) return; // 同一个集合，直接返回
+
+                // 固定把 x 的根合并到 y 的根下面（x → y）
+            parent[rootx] = rooty;
 	}
 
 	bool is_same(int x, int y) {//判断是否在同一集合
